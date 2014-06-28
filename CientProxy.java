0bb3cae59396d25ea2316b3f04bdaac44ef81f33
Adam Towers <adam.towers77@gmail.com>
2014-06-27 17:53:25 -0700

    yo3
    lo3

---
+++ CientProxy.java
@@ -0,0 +1,13 @@
+package com.zarkoix.aborealWizardry.core.client;
+
+
+import com.zarkoix.aborealWizardry.core.CommonProxy;
+
+public class ClientProxy extends CommonProxy {
+        
+        @Override
+        public void registerRenderers() {
+                // This is for rendering entities and so forth later on
+        }
+        
+}
