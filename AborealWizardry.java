19bd0233bc30453bc5e91823eb2580a8d5495c0b
Adam Towers <adam.towers77@gmail.com>
2014-06-27 17:52:06 -0700

    yo
    lo

---
+++ AborealWizardry.java
@@ -0,0 +1,44 @@
+package com.zarkoix.aborealWizardry.core;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler; // used in 1.6.2
+//import cpw.mods.fml.common.Mod.PreInit;    // used in 1.5.2
+//import cpw.mods.fml.common.Mod.Init;       // used in 1.5.2
+//import cpw.mods.fml.common.Mod.PostInit;   // used in 1.5.2
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+//import cpw.mods.fml.common.network.NetworkMod; // not used in 1.7
+
+@Mod(modid="AborealWizardry", name="AborealWizardry", version="0.0.0")
+//@NetworkMod(clientSideRequired=true) // not used in 1.7
+public class AborealWizardry {
+
+        // The instance of your mod that Forge uses.
+        @Instance(value = "AborealWizardry")
+        public static AborealWizardry instance;
+        
+        // Says where the client and server 'proxy' code is loaded.
+        @SidedProxy(clientSide="com.zarkoix.aborealWizardry.core.client.ClientProxy", serverSide="com.zarkoix.aborealWizardry.core.CommonProxy")
+        public static CommonProxy proxy;
+        
+        @EventHandler // used in 1.6.2
+        //@PreInit    // used in 1.5.2
+        public void preInit(FMLPreInitializationEvent event) {
+                // Stub Method
+        }
+        
+        @EventHandler // used in 1.6.2
+        //@Init       // used in 1.5.2
+        public void load(FMLInitializationEvent event) {
+                proxy.registerRenderers();
+        }
+        
+        @EventHandler // used in 1.6.2
+        //@PostInit   // used in 1.5.2
+        public void postInit(FMLPostInitializationEvent event) {
+                // Stub Method
+        }
+}
